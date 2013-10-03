0d7554f65b847508e7098c56e6b4968b0e7f1fee
dzinder <dzinder@CSCS-BIOLAB2.adsroot.itcs.umich.edu>
2013-10-03 14:15:02 -0400

    Added neutral segments. Only the first nImmunogenicSegments are used for calculating specific immunity. riskOfInfection = exp( -xi_gen * n_previous_infections +                        -xi_spec * n_unencountered_immunogenic_segments / nImmunogenicSegmets )

--- src/Parameters.java
+++ src/Parameters.java
@@ -13,79 +13,81 @@ enum PhenotypeType {SEGMENTED};
 enum VaccineMakeup {PREVALENT_SEGMENTS, PREVALENT_STRAINS, MAXIMUM_COVERAGE};
 
 public class Parameters {
-			
+
 	// Simulation Parameters
 	@Setting (description ="Burn In time in days. Initial run time without recording output."	) 
-		static int burnin = 365*50; 	
+	static int burnin = 365*50; 	
 	@Setting (description ="Simulation end time in days."	) 
-		static int endDay = 365*90; 	
+	static int endDay = 365*90; 	
 	@Setting (description ="Repeat simulation following a stochastic extinction until endDay is reached."	) 
-		static boolean repeatSim = true;		
+	static boolean repeatSim = true;		
 	@Setting (description ="Prevent stochastic extinction during burn-in period by maintaining at least one infected individual...." ) 
-		static boolean keepAliveDuringBurnin = true; 	
+	static boolean keepAliveDuringBurnin = true; 	
 	@Setting (description = "Prevent stochastic extinction by maintaining at least one infected individual.... " )
-		static boolean keepAlive = true; 
-	
+	static boolean keepAlive = true; 
+
 	// Sampling and Tree Sampling Parameters	
 	@Setting (description ="print to out.timeseries every X days.")
-		static int printStepTimeseries = 7;	
+	static int printStepTimeseries = 7;	
 	@Setting (description ="print to out.immunity every X days")
-		static int printStepImmunity = 7;						
+	static int printStepImmunity = 7;						
 	@Setting (description ="in proportion samples per day")
-		static double tipSamplingRate = 1E-4;				    
+	static double tipSamplingRate = 1E-4;				    
 	@Setting (description ="true to set sampling proportional to prevalence (vs. population size)" )
-		static boolean tipSamplingProportional = false;		
+	static boolean tipSamplingProportional = false;		
 	@Setting (description ="proportion of tips to use in tree reconstruction" )
-		static double treeProportion = 1E-1;	
+	static double treeProportion = 1E-1;	
 	@Setting (description ="interval used for sampling subset of tips to be marked" ) 
-		static double intervalForMarkTips = 10;
+	static double intervalForMarkTips = 10;
 	@Setting (description ="how many tips to sample when estimating diversity" )
-		static int	diversitySamplingCount = 50;
+	static int	diversitySamplingCount = 50;
 	@Setting (description ="subtract this many years off the end of the tree when designating trunk" )
-		static double yearsToTrunk = 5;			
+	static double yearsToTrunk = 5;			
 	@Setting (description = "Sample whole genomes for tips rather than random samples.... " )
-		static boolean sampleWholeGenomes = true; 
+	static boolean sampleWholeGenomes = true; 
 	@Setting (description = "Infected host sampling rate for out.infected" )
-		static double infectedHostSamplingRate = 1E-3; 
+	static double infectedHostSamplingRate = 1E-3; 
 	@Setting (description = "Host sampling rate for out.immunity" )
-		static double immunityHostSamplingRate = 1E-4;
-	
+	static double immunityHostSamplingRate = 1E-4;
+
 
 	// Host & Host Population Parameters & Settings
 	@Setting (description ="Number of hosts in population" )
-		static int N = 300000;								
+	static int N = 300000;								
 	@Setting (description ="in births per individual per day, i.e. 1/(30*365)" )
-		static double birthRate = 1.0/(30.0*365.0);	
+	static double birthRate = 1.0/(30.0*365.0);	
 	@Setting (description ="in deaths per individual per day, i.e. 1/(30*365)" )
-		static double deathRate = 1.0/(30.0*365.0);	
+	static double deathRate = 1.0/(30.0*365.0);	
 	@Setting (description ="whether to keep overall population size constant" )
-		static boolean swapDemography = true;				
+	static boolean swapDemography = true;				
 
 	// Infection & Epidemiology Parameters
 	@Setting (description ="initial number of infected individuals" )
-		static int initialI = 1;
+	static int initialI = 1;
 	@Setting (description ="proportion recovered to intial virus/es (multiple recoveries for value greater than 1)" )
-		static double initialPrR = 8.0; 
+	static double initialPrR = 8.0; 
 	@Setting (description ="in contacts per individual per day" )
-		static double beta = 25.0/7.0; 
+	static double beta = 25.0/7.0; 
 	@Setting (description ="in recoveries per individual per day" )
-		static double nu = 1.0/7.0; 
-	
+	static double nu = 1.0/7.0; 
+
 	// Virus Segment Parameters
 	@Setting (description ="number of viral segments" ) 
-		static int nSegments = 2;
+	static int nSegments = 4;
+	@Setting (description ="number of immunogenic segments (only these first n segments will generate effective immunity)" ) 
+	static int nImmunogenicSegments = 2;
 	@Setting (description ="number of inital segment allels" ) 
-		static int[] nInitialSegmentAllels = {5,3};
+	static int[] nInitialSegmentAllels = {1,1,1,1};
 	@Setting (description ="number of inital random viral segement combinations" ) 
-		static int nInitialStrains = 12;
-	
+	static int nInitialStrains = 1;
+
 	// Mutation & Reassortment Parameters
 	@Setting (description ="mutation rate - in mutations per infected host per day" )
-		static double mu = 0;//1E-4;
+	static double mu = 0;//1E-4;
 	@Setting (description ="reassortment probability - probability of segment to be randomly chosen from all possible segments in a superinfection" )
-		static double rho = 0;//0.1;			    
+	static double rho = 0;//0.1;			    
 	@Setting (description ="infection bottle neck size - at most number of segment combinations to be transmitted from a superinfected host" )
-		static double n_bottleNeck = 1;			     	
+	static double n_bottleNeck = 1;			     	
 
 	// Vaccine Parameters
 	@Setting (description ="vaccination ages in days (must be inputed in increasing order)" )
@@ -98,13 +100,13 @@ public class Parameters {
 	static int vaccineValancy = 2;
 	@Setting (description ="time of vaccination program start in days" )
 	static int vaccinationProgramStartTime = 365*70; // days 
-		
+
 	// Memory Optimization Parameters
 	@Setting (description ="interval at which to streamline tree (optimize memory usage)" )
-		static int treeStreamlineInterval = 5000;
-	
+	static int treeStreamlineInterval = 5000;
+
 	public static int day = 0;
-	
+
 	// measured in years, starting at burnin
 	public static float getDate() {
 		return (float) (((float) day - (float) burnin ) / 365.0 );
@@ -114,25 +116,27 @@ public class Parameters {
 
 	public static List<Virus> initialViruses = null;
 	public static Segment urSegment;
-	
+
 
 
 	public static void init() {
 
 		day=0;
-		
-		Random.init();
-		
+
 		s.apply(Random.class);
+
+		Random.init();
+
 		s.apply(ImmuneSystemDiscrete.class);
-		
+		ImmuneSystemDiscrete.updateImmunogenicSegmentMask(nImmunogenicSegments);
+
 		urSegment = new Segment(); // root to all segments		
 		initialViruses=new ArrayList<Virus>();
-		
+
 		// Construct initial segment types
 		List<Segment> initialSegmentRoots=new ArrayList<Segment>();
 		List<List<Segment>> initialSegments = new ArrayList<List<Segment>>(); 
-		
+
 		for (short i=0;i<nSegments;i++) {
 			initialSegmentRoots.add(urSegment.mutate((short)i));
 			initialSegments.add(new ArrayList<Segment>());
@@ -140,7 +144,7 @@ public class Parameters {
 				initialSegments.get(i).add(initialSegmentRoots.get(i).mutate());
 			}
 		}
-		
+
 		// Construct initial random strains
 		for (short i=0;i<nInitialStrains;i++) {
 			List<Segment> viralSegments = new ArrayList<Segment>();
@@ -156,7 +160,7 @@ public class Parameters {
 		s = new Settings(args);
 		s.apply(Parameters.class);	
 	}
-	
+
 	public static void printParams() {
 
 		try {
