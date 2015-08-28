9c534003329a94eaf66dded7a366b396fd7c0f7b
dzinder <dzinder@EE-ZINDER-DELL.hullbiolabs.uchicago.edu>
2015-08-27 20:34:47 -0500

    minor, parameter descritiption

--- src/Parameters.java
+++ src/Parameters.java
@@ -108,7 +108,7 @@ public class Parameters {
 	
 	public static class VirusParameters {
 		// Virus Parameters		
-		@Setting (description ="virus fitness EQUAL_FITNESS/SEGMENT_FITNESS/LESS_IN_CREATION (1-p1*exp(-t*p2))" ) 
+		@Setting (description ="virus fitness EQUAL_FITNESS/SEGMENT_FITNESS/INC_SINCE_CREATION (1-p1*exp(-t*p2))" ) 
 		static ViralFitnessType virusFitnessType = ViralFitnessType.EQUAL_FITNESS;
 		@Setting (description ="viral fitness parameter 1" ) 		
 		public static double viralFitnessParam1 = 0.5;
